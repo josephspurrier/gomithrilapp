4b189d670f1be1b1b697ccdfb84294dbd2fee4f9
Joseph Spurrier <github@josephspurrier.com>
2019-05-30 14:28:01 -0400

    Test running UI tests in travis

--- bash/resetdb.sh
+++ bash/resetdb.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-source $GOPATH/.env
+# If TRAVIS is not set, load from the environment variables.
+if [ -z "$TRAVIS" ]; then
+    source $GOPATH/.env
+fi  
 
 docker exec mysql56 sh -c "exec mysql -h 127.0.0.1 -uroot -p${MYSQL_ROOT_PASSWORD} -e 'DROP DATABASE IF EXISTS main;'"
 docker exec mysql56 sh -c "exec mysql -h 127.0.0.1 -uroot -p${MYSQL_ROOT_PASSWORD} -e 'CREATE DATABASE IF NOT EXISTS main DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;'"
