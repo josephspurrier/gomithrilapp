81b26490c1930139d8e0bb1b3ebd04ce5d3eda4f
Joseph Spurrier <github@josephspurrier.com>
2020-04-01 23:53:15 -0400

    Fix the name casing

--- src/app/ui/script/index.js
+++ src/app/ui/script/index.js
@@ -1,12 +1,12 @@
 import m from "mithril";
-import AboutPage from "./page/About";
-import LoginPage from "./page/Login";
-import RegisterPage from "./page/Register";
-import IndexPage from "./page/Index";
-import NotepadPage from "./page/Notepad";
-import ErrorPage from "./page/Error";
-import LayoutMain from "./page/layout/Main";
-import Auth from "./module/Auth";
+import AboutPage from "./page/about";
+import LoginPage from "./page/login";
+import RegisterPage from "./page/register";
+import IndexPage from "./page/index";
+import NotepadPage from "./page/notepad";
+import ErrorPage from "./page/error";
+import LayoutMain from "./page/layout/main";
+import Auth from "./module/auth";
 import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
 import "../style/main.scss";
 
