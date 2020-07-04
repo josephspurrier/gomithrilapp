93e2c2e129139f028123a46d1968d91b2165892a
Joseph Spurrier <jspurrier@cloudtamer.io>
2020-07-04 01:22:08 -0400

    Add ability to mock requests

--- src/app/ui/src/view/notepad.stories.js
+++ src/app/ui/src/view/notepad.stories.js
@@ -3,6 +3,7 @@ import m from "mithril";
 import { withKnobs } from "@storybook/addon-knobs";
 import { withA11y } from "@storybook/addon-a11y";
 import NotepadPage from "@/view/notepad";
+import Mock from "@/component/mock";
 import "~/style/main.scss";
 
 export default {
@@ -12,5 +13,19 @@ export default {
 };
 
 export const notepad = () => ({
+  oninit: () => {
+    Mock.success({
+      notes: [
+        {
+          id: "6e8568e5-2632-7c8d-b448-ec82772ed4ec",
+          message: "foo",
+        },
+        {
+          id: "a3969708-bf1c-efd4-9d98-d8d5a217cd93",
+          message: "bar",
+        },
+      ],
+    });
+  },
   view: () => <NotepadPage />,
 });
