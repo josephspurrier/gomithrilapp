4f3938611dafb71e6d261fa2b28ac581fe7afeef
Joseph Spurrier <github@josephspurrier.com>
2020-07-07 21:53:11 -0400

    Convert more to closures

--- src/app/ui/src/view/login.js
+++ src/app/ui/src/view/login.js
@@ -7,102 +7,100 @@ var data = {
   subtitle: "Enter your login information below.",
 };
 
-var clear = (vnode) => {
-  vnode.state.user = {};
-};
+var Page = (vnodeInitial) => {
+  let user = {};
 
-var Page = {
-  oninit: (vnode) => {
-    clear(vnode);
+  var clear = () => {
+    user = {};
+  };
 
-    // Prefill the fields.
-    if (vnode.attrs.email) {
-      vnode.state.user.email = vnode.attrs.email;
-    }
-    if (vnode.attrs.password) {
-      vnode.state.user.password = vnode.attrs.password;
-    }
-  },
-  onremove: (vnode) => {
-    clear(vnode);
-  },
-  view: (vnode) => (
-    <main>
-      <div>
-        <section class="section">
-          <div class="container">
-            <h1 class="title">{data.title}</h1>
-            <h2 class="subtitle">{data.subtitle}</h2>
-          </div>
+  // Prefill the fields.
+  if (vnodeInitial.attrs.email) {
+    user.email = vnodeInitial.attrs.email;
+  }
+  if (vnodeInitial.attrs.password) {
+    user.password = vnodeInitial.attrs.password;
+  }
 
-          <div class="container" style="margin-top: 1em;">
-            <form
-              name="login"
-              onsubmit={(e) => {
-                UserLogin(e, vnode.state.user)
-                  .then(() => {
-                    clear(vnode);
-                  })
-                  .catch(() => {});
-              }}
-            >
-              <Input
-                label="Email"
-                name="email"
-                required="true"
-                oninput={(e) => {
-                  vnode.state.user.email = e.target.value;
-                }}
-                value={vnode.state.user.email}
-              />
+  return {
+    view: (vnode) => (
+      <main>
+        <div>
+          <section class="section">
+            <div class="container">
+              <h1 class="title">{data.title}</h1>
+              <h2 class="subtitle">{data.subtitle}</h2>
+            </div>
 
-              <Input
-                label="Password"
-                name="password"
-                required="true"
-                oninput={(e) => {
-                  vnode.state.user.password = e.target.value;
-                }}
-                value={vnode.state.user.password}
-                type="password"
-              />
-
-              <div class="field is-grouped">
-                <p class="control">
-                  <button
-                    id="submit"
-                    type="submit"
-                    data-cy="submit"
-                    class="button is-primary"
-                  >
-                    Submit
-                  </button>
-                </p>
-
-                <p class="control">
-                  <button
-                    type="button"
-                    class="button is-light"
-                    onclick={() => {
+            <div class="container" style="margin-top: 1em;">
+              <form
+                name="login"
+                onsubmit={(e) => {
+                  UserLogin(e, user)
+                    .then(() => {
                       clear(vnode);
-                    }}
-                  >
-                    Clear
-                  </button>
-                </p>
+                    })
+                    .catch(() => {});
+                }}
+              >
+                <Input
+                  label="Email"
+                  name="email"
+                  required="true"
+                  oninput={(e) => {
+                    user.email = e.target.value;
+                  }}
+                  value={user.email}
+                />
 
-                <p class="control">
-                  <m.route.Link href="/register" class="button is-light">
-                    Register
-                  </m.route.Link>
-                </p>
-              </div>
-            </form>
-          </div>
-        </section>
-      </div>
-    </main>
-  ),
+                <Input
+                  label="Password"
+                  name="password"
+                  required="true"
+                  oninput={(e) => {
+                    user.password = e.target.value;
+                  }}
+                  value={user.password}
+                  type="password"
+                />
+
+                <div class="field is-grouped">
+                  <p class="control">
+                    <button
+                      id="submit"
+                      type="submit"
+                      data-cy="submit"
+                      class="button is-primary"
+                    >
+                      Submit
+                    </button>
+                  </p>
+
+                  <p class="control">
+                    <button
+                      type="button"
+                      class="button is-light"
+                      onclick={() => {
+                        clear(vnode);
+                      }}
+                    >
+                      Clear
+                    </button>
+                  </p>
+
+                  <p class="control">
+                    <m.route.Link href="/register" class="button is-light">
+                      Register
+                    </m.route.Link>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </section>
+        </div>
+      </main>
+    ),
+  };
 };
 
 export default Page;
--- src/app/ui/src/view/notepad.js
+++ src/app/ui/src/view/notepad.js
@@ -2,67 +2,69 @@ import m from "mithril"; // eslint-disable-line no-unused-vars
 import NoteStore from "@/store/notestore";
 import Note from "@/component/note";
 
-var Page = {
-  oninit: () => {
-    NoteStore.load();
-  },
-  onremove: () => {
-    NoteStore.clear();
-  },
-  view: () => (
-    <section id="note-section" class="section">
-      <div class="container">
-        <div class="box">
-          <div class="field">
-            <label class="label">To Do</label>
-            <div class="control">
-              <input
-                v-model="inputTodo"
-                type="text"
-                placeholder="What would you like to do?"
-                class="input"
-                name="note-add"
-                data-cy="note-text"
-                onkeypress={(e) => {
-                  if (e.key !== "Enter") {
-                    return;
-                  }
-                  NoteStore.submit();
-                }}
-                oninput={(e) => {
-                  NoteStore.current.message = e.target.value;
-                }}
-                value={NoteStore.current.message}
-              />
+var Page = () => {
+  NoteStore.load();
+
+  return {
+    view: () => (
+      <section id="note-section" class="section">
+        <div class="container">
+          <div class="box">
+            <div class="field">
+              <label class="label">To Do</label>
+              <div class="control">
+                <input
+                  v-model="inputTodo"
+                  type="text"
+                  placeholder="What would you like to do?"
+                  class="input"
+                  name="note-add"
+                  data-cy="note-text"
+                  onkeypress={(e) => {
+                    if (e.key !== "Enter") {
+                      return;
+                    }
+                    NoteStore.submit();
+                  }}
+                  oninput={(e) => {
+                    NoteStore.current.message = e.target.value;
+                  }}
+                  value={NoteStore.current.message}
+                />
+              </div>
             </div>
+            <nav class="level is-mobile">
+              <div class="level-left">
+                <a
+                  title="Add note"
+                  class="level-item"
+                  onclick={NoteStore.submit}
+                >
+                  <span class="icon is-small has-text-success">
+                    <i class="far fa-plus-square" data-cy="add-note-link"></i>
+                  </span>
+                </a>
+              </div>
+            </nav>
           </div>
-          <nav class="level is-mobile">
-            <div class="level-left">
-              <a title="Add note" class="level-item" onclick={NoteStore.submit}>
-                <span class="icon is-small has-text-success">
-                  <i class="far fa-plus-square" data-cy="add-note-link"></i>
-                </span>
-              </a>
-            </div>
-          </nav>
+          <div>
+            <ul id="listTodo">
+              {NoteStore.list.map((note) => (
+                <Note
+                  key={note.id}
+                  id={note.id}
+                  message={note.message}
+                  oninput={(e) => {
+                    note.message = e.target.value;
+                  }}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
-        <div>
-          <ul id="listTodo">
-            {NoteStore.list.map((note) => (
-              <Note
-                key={note.id}
-                id={note.id}
-                message={note.message}
-                oninput={(e) => {
-                  note.message = e.target.value;
-                }}
-              />
-            ))}
-          </ul>
-        </div>
-      </div>
-    </section>
-  ),
+      </section>
+    ),
+  };
 };
 
 export default Page;
