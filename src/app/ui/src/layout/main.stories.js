ae6a90245fc6a1717c35a54ee93c29895b89ea31
Joseph Spurrier <github@josephspurrier.com>
2020-07-07 22:23:28 -0400

    Add remaining stories

---
+++ src/app/ui/src/layout/main.stories.js
@@ -0,0 +1,25 @@
+import m from "mithril"; // eslint-disable-line no-unused-vars
+import { withKnobs, text } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
+import LayoutMain from "./main";
+import SimplePage from "@/component/simple-page";
+import "~/style/main.scss";
+
+export default {
+  title: "Component/Layout Main",
+  component: LayoutMain,
+  decorators: [withKnobs, withA11y],
+};
+
+export const simplePage = () => ({
+  view: () => {
+    return m(
+      LayoutMain,
+      m(SimplePage, {
+        title: text("Title", "This is the Title"),
+        description: text("Description", "This is a subtitle or description."),
+        content: text("Content", "This is the content."),
+      })
+    );
+  },
+});
